c8e66a7fdfa629ccf6ea60f042a71c8162fc660a
Thomas Daley <prayerproduction@gmail.com>
2023-01-17 20:06:40 +0000

    implemented starting search feature

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import { API_URL } from "../../utils/Api";
 const Main = () => {
   const [spaceCrafts, setSpaceCrafts] = useState(null);
+  const [searchValue, setSearchValue] = useState("");
 
   const getData = async () => {
     const { data } = await axios.get(API_URL);
@@ -18,13 +19,27 @@ const Main = () => {
   if (!spaceCrafts) {
     return <p>...loading</p>;
   } else {
+    const searchedSpaceCrafts = spaceCrafts.filter((spaceCrafts) =>
+      spaceCrafts.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
     return (
       <div className="main">
-        <div className="Spacecrafts">
-          {spaceCrafts.map((spaceCraft) => {
+        <div className="container">
+          <label htmlFor="search">Search</label>
+          <input
+            value={searchValue}
+            onChange={(event) => {
+              setSearchValue(event.target.value);
+            }}
+            id="search"
+            name="search"
+            type="text"
+          />
+          {searchedSpaceCrafts.map((spaceCraft) => {
             return (
-              <div key={spaceCraft.uid}>
-                <p>Name: {spaceCraft.name}</p>
+              <div className="spacecrafts" key={spaceCraft.uid}>
+                <div className=""></div>
+                {spaceCraft.name && <p>Name: {spaceCraft.name}</p>}
                 {spaceCraft.registry && <p>Registry: {spaceCraft.registry}</p>}
                 {spaceCraft.status && <p>Status: {spaceCraft.status}</p>}
                 {spaceCraft.dateStatus && (
