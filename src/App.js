22185eacf3c61a0ee2a134dfa04af7cbbc52fd33
Thomas Daley <prayerproduction@gmail.com>
2023-01-17 18:42:08 +0000

    added ternary logic for api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Main from "./components/Main/Main";
-
+import "./styles/global.scss";
 function App() {
   return (
     <div className="App">
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { API_URL } from "../../utils/Api";
 const Main = () => {
-  const [spaceCrafts, setSpaceCrafts] = useState({});
+  const [spaceCrafts, setSpaceCrafts] = useState(null);
 
   const getData = async () => {
     const { data } = await axios.get(API_URL);
@@ -17,9 +17,35 @@ const Main = () => {
 
   if (!spaceCrafts) {
     return <p>...loading</p>;
+  } else {
+    return (
+      <div className="main">
+        <div className="Spacecrafts">
+          {spaceCrafts.map((spaceCraft) => {
+            return (
+              <div key={spaceCraft.uid}>
+                <p>Name: {spaceCraft.name}</p>
+                {spaceCraft.registry && <p>Registry: {spaceCraft.registry}</p>}
+                {spaceCraft.status && <p>Status: {spaceCraft.status}</p>}
+                {spaceCraft.dateStatus && (
+                  <p>dateStatus: {spaceCraft.dateStatus}</p>
+                )}
+
+                {spaceCraft.spacecraftClass && (
+                  <p>spacecraftClass: {spaceCraft.spacecraftClass.name}</p>
+                )}
+
+                {spaceCraft.owner && <p>Owner: {spaceCraft.owner.name}</p>}
+                {spaceCraft.operator && (
+                  <p>Owner: {spaceCraft.operator.name}</p>
+                )}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
   }
-  console.log(spaceCrafts);
-  return <div></div>;
 };
 
 export default Main;
